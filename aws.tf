7ecf8201c00cc1f7cab214200c0e8016e5d707d3
jennszn11 <92038804+jennszn11@users.noreply.github.com>
2021-10-06 13:57:11 -0400

    Restricted to allow personal IP
    Restricted to allow personal IP and not open to 0.0.0.0/0

--- aws.tf
+++ aws.tf
@@ -34,9 +34,9 @@ module "security_group_1" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "~> 3.0"
   name                = "security_group_spoke1"
-  description         = "Security group for example usage with EC2 instance"
+  description         = "Security group for example usage with EC2 instance jenn"
   vpc_id              = module.aws_spoke_1.vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["68.40.168.230/32"]
   ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
   providers = {
